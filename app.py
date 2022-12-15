02c694c9f7778231e383635a6914c6574e686daa
PASSGrade <120510645+PASSGrade@users.noreply.github.com>
2022-12-15 11:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.title("Collage Project\n")
-st.title("Email/Sms Spam Classifier")
+st.title("College Project\n")
+st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_input("Enter the SmS")
+input_sms = st.text_input("Enter the SMS")
 
 if st.button('Predict'):
     # 1. preprocess
